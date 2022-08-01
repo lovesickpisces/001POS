c85381f789c0020046b4ac2d7d51ebca3f345035
Lucas Maki <braydenmaki@icloud.com>
2022-07-31 22:04:10 -0700

    Update main.go

--- main.go
+++ main.go
@@ -11,14 +11,27 @@ import (
 	"time"
 )
 
-// type employee struct {
-// 	name        string
-// 	pin         string
-// 	displayName string
-// 	phoneNumber string
-// 	clockIn     time.Time
-// 	clockout    time.Time
-// }
+func leaveCheck(input string) {
+	upperInput := strings.ToUpper(input)
+	switch upperInput {
+	case "Q":
+		os.Exit(0)
+	case "QUIT":
+		os.Exit(0)
+	case "E":
+		os.Exit(0)
+	case "EXIT":
+		os.Exit(0)
+	}
+}
+
+func clockInPrintout(name string) {
+	dt := time.Now()
+	fmt.Printf("Username: %s\n", name)
+	fmt.Printf("Clocked in at: %s\n", dt.Format(time.RFC822))
+	println("hours worked xx/xx-xx/xx : TBA")
+
+}
 
 func findEmployee(employeePin string) {
 	infile, err := os.Open("Employee.csv")
@@ -42,16 +55,19 @@ func findEmployee(employeePin string) {
 			input = strings.Replace(input, "\n", "", -1)
 			if strings.Compare(input, "y") == 0 {
 				logClockIn(record[1] + "," + record[2])
+				clockInPrintout(record[2])
 				break
 			} else if strings.Compare(input, "n") == 0 {
 				fmt.Println("Okay :)")
 				break
 			} else {
+				leaveCheck(input)
 				break
 			}
 		}
 	}
 }
+
 func logClockIn(e string) {
 	dt := time.Now()
 	todayDt := dt.Format("2006-01-02")
@@ -80,6 +96,7 @@ func main() {
 		input, _ := reader.ReadString('\n')
 		input = strings.Replace(input, "\n", "", -1)
 		if strings.Compare(input, "") != 0 {
+			leaveCheck(input)
 			findEmployee(input)
 		}
 	}
