c6ab4adbdaeb8d9ed1f17a99e80c20dad2365222
Lucas Maki <braydenmaki@icloud.com>
2022-08-10 13:40:02 -0700

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -83,56 +84,66 @@ func findEmployee(employeePin string) {
 		if strings.Compare(employeePin, record[1]) == 0 {
 
 			/* Now we read in if the user wants to clock
-			 * in (or out). We use 'reader'	as a
-			 * *bufio.Reader type for the user input and
-			 * we go through the checks 				*/
-			reader := bufio.NewReader(os.Stdin)
-			fmt.Printf("do you want to clock in %s? (y/n)", record[2])
-			fmt.Print("-> ")
-			input, _ := reader.ReadString('\n')
-			input = strings.Replace(input, "\n", "", -1)
+			 * in (or out).								*/
+			input := getFeedback("do you want to clock in " + record[2] + " (y/n)")
 
 			/* If the User wants to clock in we initalize
 			 * 'e' as an EmployeeShift  type and fill it
 			 * with the necessary information. Then we
-			 * storethe employee while they are clocked
+			 * store the employee while they are clocked
 			 * in and give the user a clock in printout.*/
 			if strings.Compare(input, "y") == 0 {
-				var employee = logClockIn(record[0], record[1], record[2])
-				storeEmployee(employee)
+
+				/* Here is the jobs part*/
+				count := 1
+				jobsList := getEmployeeJobs(employeePin)
+				jobInput := getFeedback("Please type the number of the job you are working today")
+				var employeeJob string
+				if strings.Compare(jobInput, "") != 0 {
+					for i := 0; i < 7; i++ {
+						if strings.Compare(jobsList[i], "n") == 0 {
+							i++
+						} else {
+							jobInputInt, err := strconv.Atoi(jobInput)
+							if err != nil {
+								log.Fatal(err)
+							} else if count == jobInputInt {
+								employeeJob = jobsList[i]
+							}
+							count++
+						}
+					}
+				}
+				var employee = logClockIn(record[0], record[1], record[2], employeeJob)
 				clockInPrintout(employee)
 				break
 			} else if strings.Compare(input, "n") == 0 {
 				fmt.Println("Okay :)")
 				break
-			} else {
-				leaveCheck(input)
-				break
 			}
-
 		}
 	}
 }
 
 func getDailyLog() string {
 	todayDt := time.Now().Format("2006-01-02")
-	todayDt = todayDt + "-log.csv"
+	todayDt = "dailyLogs/" + todayDt + "-log.csv"
 	return todayDt
 }
-func logClockIn(newName, newPin, newUser string) EmployeeShift {
-	/*													*/
+func logClockIn(newName, newPin, newUser, employeeJob string) EmployeeShift {
+
 	/* This starts off pretty simple with getting the
 	 * log file name  then filling up the variable
 	 * 'employee' of type EmployeeShift. 				*/
 	dt := time.Now()
 	filename := getDailyLog()
 	employee := EmployeeShift{
-		pin:      newPin,     // String
-		username: newUser,    // String
-		sales:    0,          // float32
-		ccTips:   0,          // float32
-		clockIn:  dt,         // time.Time
-		job:      "Your Job", // String
+		pin:      newPin,      // String
+		username: newUser,     // String
+		sales:    0,           // float32
+		ccTips:   0,           // float32
+		clockIn:  dt,          // time.Time
+		job:      employeeJob, // String
 	}
 
 	/* We check if the log file exists or need to create
@@ -145,10 +156,8 @@ func logClockIn(newName, newPin, newUser string) EmployeeShift {
 	/* We write pin,username,hh:mm:ss,clockIn/clockOut
 	 * into the log file. The pin is going to be used as
 	 * A key later which is why we aere logging it here	*/
-
-	var employeeInfo string
-	employeeInfo = employee.pin + "," + employee.username +
-		"," + employee.clockIn.Format("15:04:05") + ",Clocked IN" + "\n"
+	employeeInfo := employee.pin + "," + employee.username +
+		"," + employee.clockIn.Format("15:04:05") + ",Clocked IN" + "," + employee.job + "\n"
 
 	if _, err := outFile.Write([]byte(employeeInfo)); err != nil {
 		outFile.Close()
@@ -159,22 +168,78 @@ func logClockIn(newName, newPin, newUser string) EmployeeShift {
 	}
 	return employee
 }
+func getEmployeeJobs(employeePin string) [7]string {
 
-func storeEmployee(e EmployeeShift) {
+	/* This begins with us setting up an array to be
+	 * returned and an array with the strings to assign
+	 * later to the returned array . There is also a
+	 * Count used to keep track of how many to list		*/
+	var returnedJobs [7]string
+	var jobs [7]string
+	jobs[0] = "Manager"
+	jobs[1] = "Server"
+	jobs[2] = "Bartender"
+	jobs[3] = "Kitchen"
+	jobs[4] = "Salary Manager"
+	jobs[5] = "Salary Kitchen"
+	jobs[6] = "FOH Support"
+	count := 0
 
+	/* Here we access the jobs.csv file that keeps track
+	 * who has what job. 								*/
+	infile, err := os.Open("jobs.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/* Kind of just repeating what we did to go through
+	 * CSVs in the other functions						*/
+	jobsCSV := csv.NewReader(infile)
+	for {
+		record, err := jobsCSV.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if strings.Compare(employeePin, record[0]) == 0 {
+			fmt.Println()
+			for i := 2; i <= 8; i++ {
+				if strings.Compare(record[i], "y") == 0 {
+					fmt.Printf("(%d) %s \n", count+1, jobs[i-2])
+					returnedJobs[count] = jobs[i-2]
+					count++
+				} else {
+					returnedJobs[i-2] = "n"
+					// count++
+				}
+			}
+			break
+		}
+	}
+	return returnedJobs
+}
+
+/*														*/
+/* This function just helps minamize the amount of times
+ * You need to check input and everything :)			*/
+func getFeedback(prompt string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("\nType (q) to quit.\n")
+	fmt.Printf("%s\n", prompt)
+	fmt.Print("-> ")
+	input, _ := reader.ReadString('\n')
+	input = strings.Replace(input, "\n", "", -1)
+	leaveCheck(input)
+	return input
 }
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
 	loadingScreen()
 	for {
-		fmt.Print("\n\n\nType (q) to quit.\n")
-		fmt.Print("Enter an employee pin to clock in/out\n")
-		fmt.Print("-> ")
-		input, _ := reader.ReadString('\n')
-		input = strings.Replace(input, "\n", "", -1)
+		input := getFeedback("Enter an employee pin to clock in/out")
 		if strings.Compare(input, "") != 0 {
-			leaveCheck(input)
 			findEmployee(input)
 		}
 	}
